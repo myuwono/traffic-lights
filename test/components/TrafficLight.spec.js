9cd1cc99ff0bb25ba516dd9a46d7ec977ae9c315
Mitchell Yuwono <mitchellyuwono@gmail.com>
2017-03-06 02:39:42 +1100

    use mount instead of shallow (#3)

--- test/components/TrafficLight.spec.js
+++ test/components/TrafficLight.spec.js
@@ -1,5 +1,5 @@
 import TrafficLight from '../../src/components/TrafficLight'
-import {shallow} from 'enzyme'
+import {mount} from 'enzyme'
 import React from 'react'
 
 describe('TrafficLight', () => {
@@ -7,7 +7,7 @@ describe('TrafficLight', () => {
   it('should render a div with .trafficlight.{name}.{light} classes', () => {
     let name = "N"
     let light = "green" 
-    const trafficLight = shallow(<TrafficLight name={name} light={light} />)
+    const trafficLight = mount(<TrafficLight name={name} light={light} />)
     expect(trafficLight.find(`.trafficlight.${name}.${light}`).exists())
   });
 
