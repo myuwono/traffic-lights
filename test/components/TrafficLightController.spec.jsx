4ea84c1ade10de3bf9bfef043d2ec113ad89da11
Mitchell Yuwono <mitchell.yuwono@covata.com>
2017-03-06 12:07:03 +1100

    add test to assert rendering of traffic lights (#5)

--- test/components/TrafficLightController.spec.jsx
+++ test/components/TrafficLightController.spec.jsx
@@ -1,8 +1,33 @@
-import TrafficLightController from '../../src/components/TrafficLightController'
+import {mount} from 'enzyme';
+import React from 'react';
+import TrafficLightController from
+    '../../src/components/TrafficLightController';
+import TrafficLight from '../../src/components/TrafficLight';
 
+describe('TrafficLightController', () => {
+  var timerCallback;
 
-describe('TrafficLight', () => {
-  it('should render to the DOM', function() {
-    expect("hello").toEqual("hello");
+  beforeEach(function() {
+    timerCallback = jasmine.createSpy("timerCallback");
+    jasmine.clock().install();
+  });
+
+  afterEach(function() {
+    jasmine.clock().uninstall();
+  });
+
+  it('should render the expected traffic lights to the DOM', function() {
+    const trafficLightController = mount(
+      <TrafficLightController period={500} yellowInterval={1} />);
+    let trafficLights = trafficLightController.find(TrafficLight);
+    let expectedTrafficLights = [
+      <TrafficLight name="N" light="Green" />,
+      <TrafficLight name="S" light="Green" />,
+      <TrafficLight name="E" light="Red" />,
+      <TrafficLight name="W" light="Red" />,
+    ];
+    expect(trafficLights.length).toEqual(4);
+    expectedTrafficLights.forEach(trafficLight =>
+        expect(trafficLights.contains(trafficLight)).toBeTruthy());
   });
 });
